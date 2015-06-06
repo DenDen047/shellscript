5d7d009d0039de4e754ce44082389c5e8a9e4a5a
Naoya <naoyashop@yahoo.co.jp>
2015-06-06 21:09:18 +0900

    mkfile cutName.sh

---
+++ cutName.sh
@@ -0,0 +1,18 @@
+#!bin/zsh
+
+Books=~/Downloads/Manga
+
+# cut OuthorName
+Books/\(成年コミック* | cut -d [ -f2 | cut -d ] -f1 > NameList.txt
+
+# mkdir
+names=Books/NameList.txt
+while read line
+do
+    echo $line
+    mkdir Books/$line
+done < $names
+
+
+
+
