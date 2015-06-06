17099e7d703e51760e03e1e2b577213ba85416cd
Naoya <naoyashop@yahoo.co.jp>
2015-06-06 21:34:06 +0900

    add ZipFile move function

--- cutName.sh
+++ cutName.sh
@@ -2,15 +2,18 @@
 
 Books=~/Downloads/Manga
 
+mkdir $Books/zip
+mv $Books/*.zip $Books/zip
+
 # cut OuthorName
-Books/\(成年コミック* | cut -d [ -f2 | cut -d ] -f1 > NameList.txt
+$Books/\(成年コミック* | cut -d [ -f2 | cut -d ] -f1 > NameList.txt
 
 # mkdir
-names=Books/NameList.txt
+names=$Books/NameList.txt
 while read line
 do
     echo $line
-    mkdir Books/$line
+    mkdir $Books/$line
 done < $names
 
 
