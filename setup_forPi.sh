ca7e55c25fe66f40aedb80389b252b28f6df99e6
naoya <naoyashop@yahoo.co.jp>
2015-06-11 06:39:53 +0000

    shell lang US

--- setup_forPi.sh
+++ setup_forPi.sh
@@ -6,6 +6,8 @@ ln -s ~/dotfiles/.vimrc ~/.vimrc
 
 echo "add Japanese Font"
 sudo localedef -f UTF-8 -i ja_JP ja_JP
+export LC_CTYPE=en_US.UTF-8
+export LC_ALL=en_US.UTF-8
 
 echo "install softwares..."
 sudo apt-get install vim
