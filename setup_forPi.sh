fb93169018ac279c03948782637ac2f47e002ee4
naoya <naoyashop@yahoo.co.jp>
2015-06-11 05:55:56 +0000

    :

--- setup_forPi.sh
+++ setup_forPi.sh
@@ -1,6 +1,19 @@
 #!bin/bash
 
+echo "creat SymbolicLink"
+ln -s ~/dotfiles/.zshenv ~/.zshenv
+ln -s ~/dotfiles/.vimrc ~/.vimrc
+
+echo "add Japanese Font"
+sudo localedef -f UTF-8 -i ja_JP ja_JP
+
 echo "install softwares..."
 sudo apt-get install vim
 sudo apt-get install tmux
+sudo apt-get install zsh
+
+
+echo "update firmware..."
+sudo rpi-update
+sudo reboot
 
