d3520e75da96b711631e3163c60169908d1f2c7d
naoya <naoyashop@yahoo.co.jp>
2015-06-25 05:53:49 +0000

    edit importCommands

--- commands/importantCommands_forPi.sh
+++ commands/importantCommands_forPi.sh
@@ -1,2 +1,2 @@
 # Start MJPG-streamer
-sudo ~/Documents/mjpg_streamer/mjpg_streamer -i "./input_uvc.so -f 24 -r 320x240 -d /dev/video0 -y" -o "./output_http.so -w ./www -p 8080"
+sudo ~/Documents/mjpg_streamer/mjpg_streamer -i "~/Documents/mjpg_streamer/input_uvc.so -f 24 -r 320x240 -d /dev/video0 -y" -o "~/Documents/mjpg_streamer/output_http.so -w ~/Documents/mjpg_streamer/www -p 8080"
