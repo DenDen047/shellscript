cd8e64bd7a4707a0363c2ac51cd5fbb054251f0a
Naoya <naoyashop@yahoo.co.jp>
2015-07-05 20:04:51 +0900

    code look nice

--- cutName.sh
+++ cutName.sh
@@ -4,19 +4,25 @@ Download=~/Downloads
 Books=~/Downloads/Manga
 Pics=~/Downloads/Picture
 
+#-------------------
 # isolate zip files
+#-------------------
 mkdir $Books/zip
 mv $Download/*.zip $Books/zip
 mv $Books/*.zip $Books/zip
 
+#-------------------
 # cut OuthorName
+#-------------------
 echo "Make NameList.txt"
 ZipFiles=~/Downloads/Manga/zip
 ls $ZipFiles/\(成年コミック* | cut -d [ -f2 | cut -d ] -f1 > $ZipFiles/NameList.txt
 ls $ZipFiles/\[*]* | cut -d [ -f2 | cut -d ] -f1 | cut -d "(" -f1 > $ZipFiles/NameList.txt
 ls $ZipFiles/\(同人CG集* | cut -d [ -f2 | cut -d  ] -f1 > $ZipFiles/CGNameList.txt
 
+#-------------------
 # mkdir
+#-------------------
 echo "Make directories"
 names=$ZipFiles/NameList.txt
 while read line
@@ -37,7 +43,9 @@ do
 done < $names
 
 
+#-------------------
 # Finish
+#-------------------
 rm $ZipFiles/NameList.txt
 rm $ZipFiles/CGNameList.txt
 echo "Finish!!"
