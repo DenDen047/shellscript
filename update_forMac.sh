c78104452619c967cd295807553a744d310fad96
sh.mn.nat@gmail.com <naoyashop@yahoo.co.jp>
2015-11-08 13:23:52 +0900

    update

--- update_forMac.sh
+++ update_forMac.sh
@@ -32,21 +32,22 @@ brew cask update
 #    brew cask install $a
 #  fi
 #done
-caskroom="/opt/homebrew-cask/Caskroom"
-apps=($(brew cask list))
-for a in ${apps[@]};do
-  info=$(brew cask info $a)
-  if echo "$info"| grep -q "Not installed";then
-    brew cask install $a
-  fi
-  current=$(echo "$info"|grep "${caskroom}/${a}"|cut -d' ' -f1)
-  for dir in $(ls ${caskroom}/${a});do
-    testdir="${caskroom}/${a}/${dir}"
-    if [ "$testdir" != "$current" ];then
-      rm -rf "$testdir"
-    fi
-  done
-done
+
+#caskroom="/opt/homebrew-cask/Caskroom"
+#apps=($(brew cask list))
+#for a in ${apps[@]};do
+#  info=$(brew cask info $a)
+#  if echo "$info"| grep -q "Not installed";then
+#    brew cask install $a
+#  fi
+#  current=$(echo "$info"|grep "${caskroom}/${a}"|cut -d' ' -f1)
+#  for dir in $(ls ${caskroom}/${a});do
+#    testdir="${caskroom}/${a}/${dir}"
+#    if [ "$testdir" != "$current" ];then
+#      rm -rf "$testdir"
+#    fi
+#  done
+#done
 
 echo "\ncleanuping..."
 brew prune
