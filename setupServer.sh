4d6663ff94a8c0a7fac26c3f196b43e6d74aca9d
naoya <naoyashop@yahoo.co.jp>
2016-01-08 22:29:34 +0900

    edit setupserver

--- setupServer.sh
+++ setupServer.sh
@@ -1,7 +1,10 @@
 #!/bin/zsh
 
+echo "apt-get update..."
 sudo apt-get update
 sudo apt-get upgrade
 
+echo "install software for server..."
 sudo apt-get install apache2
+sudo apt-get install php5 libapache2-mod-php5
 
