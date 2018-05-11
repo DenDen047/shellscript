3a7c46876bb0c798ea3707a8beb217a1df0e5e3d
Naoya Muramatsu <naoya.muramatsu@digitalnature.slis.tsukuba.ac.jp>
2018-05-11 09:07:43 +0900

    updte_forDebian.sh

--- update_forDebain.sh
+++ update_forDebain.sh
@@ -1,12 +1,14 @@
 #!/bin/zsh
 
-echo "git setting..."
-cd ~/Documents/shellscript/
-git pull
-cd ~/dotfiles/
-git pull
+# echo "git setting..."
+# cd ~/Documents/shellscript/
+# git pull
+# cd ~/dotfiles/
+# git pull
 
 echo "updating..."
+sudo apt -y update
+sudo apt -y upgrade
 sudo apt-get -y update
-sudo apt-get -Y upgrade
-pip install -U pip
+sudo apt-get -y upgrade
+# sudo pip install -U pip
